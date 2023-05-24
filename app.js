88310bc923ccbcbc4df516f5b40e4cae42467337
showkhin1997 <showkhin15@gmail.com>
2023-05-24 18:52:05 +0600

    case increase and decrease

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-document.getElementById('case-plus').addEventListener('click', function () {
+// case button handel
+function updateCaseQuantity(isIncrease) {
     const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
-})
+    let caseNumber = caseInput.value;
+    if (isIncrease == true) {
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+}
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateCaseQuantity(true);
+});
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateCaseQuantity(false);
+});
