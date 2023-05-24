89e86a81585ad0e677a9ceeee250113282549970
showkhin1997 <showkhin15@gmail.com>
2023-05-24 20:08:06 +0600

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // case button handel
-function updateCaseQuantity(isIncrease) {
-    const caseInput = document.getElementById('case-number');
+function updateCaseQuantity(product, price, isIncrease) {
+    const caseInput = document.getElementById(product + '-number');
     let caseNumber = caseInput.value;
     if (isIncrease == true) {
         caseNumber = parseInt(caseNumber) + 1;
@@ -9,14 +9,22 @@ function updateCaseQuantity(isIncrease) {
         caseNumber = parseInt(caseNumber) - 1;
     }
     caseInput.value = caseNumber;
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber * 59;
+    const caseTotal = document.getElementById(product + '-total');
+    caseTotal.innerText = caseNumber * price;
 }
 
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateCaseQuantity('phone', 1219, true);
+});
+
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateCaseQuantity('phone', 1219, false);
+});
+
 document.getElementById('case-plus').addEventListener('click', function () {
-    updateCaseQuantity(true);
+    updateCaseQuantity('case', 59, true);
 });
 
 document.getElementById('case-minus').addEventListener('click', function () {
-    updateCaseQuantity(false);
+    updateCaseQuantity('case', 59, false);
 });
